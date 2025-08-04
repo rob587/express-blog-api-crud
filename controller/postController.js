44ab25659907f1f2661f4900d3d254090a1b2b14
rob587 <robertocamm02@yahoo.com>
2025-08-04 14:46:35 +0200

    functions

--- controller/postController.js
+++ controller/postController.js
@@ -1,1 +1,17 @@
 const menu = require('../routers/post')
+
+function index (req, res){
+    
+}
+function show (req, res){
+
+}
+function store (req, res){
+
+}
+function update (req, res){
+
+}
+function destroy (req, res){
+
+}
