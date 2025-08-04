2715e91493bce9855a99a2c6ca49090c66018819
rob587 <robertocamm02@yahoo.com>
2025-08-04 22:06:10 +0200

    [upd] restituisce un oggetto specifico con show

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.static('public'))
 app.use('/', postRouter ) 
 
 
+
 app.listen(port, ()=>{
     console.log('server della mia pasticceria')
 })
--- controller/postController.js
+++ controller/postController.js
@@ -1,12 +1,36 @@
 const menu = require('../routers/post.js')
 const posts = require('../data/posts.js')
 
+
+ 
 function index (req, res){
+    // const tags = req.query.tag
+
+    // let filteredTags
+
+    // if(tags != undefined){
+    //     filteredTags = menu.filter((item)=>{
+    //         return item.tags.includes(tags.toLowerCase)
+    //     })
+    // }
+    // else{
+
+    // }
      res.json(posts)
 }
 
 function show (req, res){
-    res.send(`dettaglio del post con id: ${req.params.id}`)
+    const id = parseInt(req.params.id)
+    const finder = posts.find(item=> item.id === id)
+
+    if(!finder) {
+        return res.json({
+            erro: 'Non trovato',
+            message: 'Post non trovato'
+        })
+    }
+
+    res.send(finder)
 }
 
 function store (req, res){
